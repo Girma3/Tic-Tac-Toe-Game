c42b4ff0eb28db5cacee696150d48a1b0a2ea24a
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2023-10-23 10:31:48 +0300

    module to mark cell with player token created

--- main.js
+++ main.js
@@ -1,15 +1,121 @@
 //create a board using module pattern
-const GameBoard = (function (){
-  let board = []
-  let row = 3;
-  let column = 3;
-
-//first for loop create empty row crate  array the second for loop add column and fill those array
-  for(let i = 0; i < 3;i++){
-    board[i] = []
-    for(let j= 0; j < 3;j++){
-       board[i][j] = j
-    }
-  }
-  return{board}
+ const GameBoard = (function(){
+  let board = [];
+  const getBoard = () => board
+  
+  
+ 
+  return{getBoard}
 })();
+//factory function to make players name and mark
+const players = function(playerName,playerMark){
+ return { playerName:playerName,
+          playerMark:playerMark}
+
+}
+//module too display the cells and player mark
+
+const displayControl = (function(){
+  //render the board  with cell(buttons)
+ const myBoard = GameBoard.getBoard();
+ 
+  
+  const grid = document.querySelector('.board');
+  
+  function diaplay(){
+  
+    
+    for(let j=0;j<9;j++){
+     
+      let cellButton = document.createElement('button');
+      cellButton.classList.add('cell')
+      cellButton.className = "cell"
+      cellButton.dataset.index = j
+      //cellbutton text would be the player mark stored in array (myboard)
+      cellButton.textContent = myBoard[j]
+      
+      grid.appendChild(cellButton)
+    }
+    
+    
+  
+  }
+  
+  
+
+return{diaplay}
+})();
+
+ const GameController = (function(){
+  //initial empty cells first
+  displayControl.diaplay()
+  
+  //call the board and players factory
+  //store the player moves in array
+   let playersMoves = GameBoard.getBoard()
+   let  player = players("girma","p")
+   let playerSecond = players("enken","Q")
+   let activePlayer  = player.playerMark;
+
+   //function to mark specific cell with player token
+  function grabmark(itemToMark){
+    
+    //check all 9 cell are empty and player mark in array is less thsn 9 moves
+    if(itemToMark.textContent == "" && playersMoves.length < 9){
+      //store the moves (activeplayer mark in array called (get))
+      playersMoves.push(swithTurn())
+
+      itemToMark.textContent = activePlayer
+      }
+      
+    } 
+  
+   //funstion to return active player mark 
+   
+   function swithTurn(){
+    
+    activePlayer = activePlayer === player.playerMark ? playerSecond.playerMark : player.playerMark;
+    console.log(activePlayer)
+    return activePlayer
+   }
+  
+
+   
+   
+   
+   
+   
+   
+   
+   
+  
+  
+ 
+ 
+  
+  function addMark(e){
+    //grab the data atribute value of the clicked item and call grabmark function to mark it
+   
+    let item = document.querySelector(`[data-index='${e.target.dataset.index}']`)
+    grabmark(item)
+    //let my = document.querySelector('[data-index="6"]')
+    console.log(item)
+    console.log(playersMoves)
+    
+    
+
+  }
+ 
+  let cells = document.querySelectorAll('.cell')
+  console.log(cells)
+  cells.forEach(btn=>{
+    btn.addEventListener("click",addMark)
+  })
+  
+ 
+
+})();
+GameController
+
+
+ 
