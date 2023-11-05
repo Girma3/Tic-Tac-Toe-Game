31d728918261cbba4155eeaf018800b1bc266b3e
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2023-11-05 11:38:13 +0300

    minimax algorithm function added a for the ai

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //create a board using module pattern
  const GameBoard = (function(){
-  let board = [];
+  let board = ["","","","","","","","",""];
   const getBoard = () => board
   return{getBoard}
 })();
@@ -34,8 +34,8 @@ const displayControl = (function(){
 const players = (playerName,playerMark)=>{
   return{playerName,playerMark}
 }
-let p1 = players('gg',"x")
- let p2 = players('ai','5')
+let p1 = players('gg',"o")
+ let p2 = players('ai','x')
 
 //factory function to make players name and mark
 function GameController(){
@@ -43,7 +43,7 @@ function GameController(){
   //call the board and players factory
   //store the player moves in array
  
-  let playersMoves = GameBoard.getBoard()
+ 
  
   const players = [
     
@@ -74,11 +74,22 @@ function GameController(){
     
     // function to aaccept free cell or square 
     const playRound = (freeCell)=>{
+      
+      if(freeCell.textContent == ""){
       freeCell.textContent = getActivePlayer().playerMark
-      //update the array or store the marks to display it using display module
-      GameBoard.getBoard().push(getActivePlayer().playerMark)
-    //check winner
-   function checkwins(){
+      let board = GameBoard.getBoard()
+      //using splice method store clicked item and update the array or store the marks to display it using display module
+      
+      
+      board.splice(freeCell.dataset.index,1,getActivePlayer().playerMark)
+      
+      
+      }
+    //check winner by accepting the board and creent player
+    let board = GameBoard.getBoard()
+    let player = getActivePlayer().playerMark
+  
+   function checkwins(board,mark){
        
         const winningConditions = [
           [0, 1, 2],
@@ -91,26 +102,30 @@ function GameController(){
           [2, 4, 6]
       ];
        //grab allthe board or buttons 
-      let gameState = document.querySelectorAll('.cell')
+     
      //loop through the winningconditon array
       for (let i = 0; i <= 7; i++) {
         
         //grab the inner array or  winingcombo[0,1,2] etc..
         //and a,b and c represent the 0,1,2 respectively depend on the inner array
         const winCondition = winningConditions[i];
-        let a = gameState[winCondition[0]].textContent;
-        let b = gameState[winCondition[1]].textContent;
-        let c = gameState[winCondition[2]].textContent;
+        let a = board[winCondition[0]];
+        let b = board[winCondition[1]];
+        let c = board[winCondition[2]];
         
-        if (a === b && b === c && c != "") {
+        if (a == b && b == c && c == mark) {
             console.log(winCondition)
+            console.log(mark)
             let haa = document.querySelector('h2')
            haa.textContent = `${getActivePlayer().playerMark}`
             break
           }
        }
     }
-      checkwins()
+    let newState = GameBoard.getBoard()
+    let currentPlay = getActivePlayer().playerMark
+    console.log(newState)
+      checkwins(newState,currentPlay)
       
       swithTurn()
     
@@ -126,6 +141,7 @@ function GameController(){
   
   function ScreenControll(){
     const game = GameController()
+    
     //render the board
     displayControl.display()
     let playerturn = document.querySelector('h1');
@@ -133,14 +149,35 @@ function GameController(){
 
   const updateScreen =()=>{
       let activeplayer = game.getActivePlayer();
-      console.log(activeplayer)
+    
       playerturn.textContent = `${activeplayer.playerMark}'s turn`
     }
+    //function to loop through the board and find empty cell using random index number 
+function computerMoves(boards){
+  
+  for(let i = 0;i < boards.length;i++){
+                       
+    let random = Math.floor(Math.random()*9)
+    let spot = boards[random]
+    if(spot == ""){
+        
+    return random
+
+      }
+
+    }
+
+
+ }
 
    function handleClick(e){
     
     let cellClicked = e.target.dataset.index
     const cell =  document.querySelector(`[data-index='${e.target.dataset.index}']`)
+   
+    
+   
+    
      if(cell.textContent != "" || !cellClicked)return
      
     //if we play with ai caheck playname is "ai" active computer move else play by click
@@ -152,71 +189,139 @@ function GameController(){
      
       updateScreen()
       if(game.getActivePlayer().playerName == 'ai'){
-        game.playRound(computerMoves())
+        let currentBoard = GameBoard.getBoard();
+        let freeCell  = document.querySelector(`[data-index='${computerMoves(currentBoard)}']`);
+       
+       //when only one cell left and if we click computermove function run
+       // and we don't have free cell left to stop that use if statment 
+       if(freeCell == undefined)return
+       
+        game.playRound(freeCell);
+      
+      
           updateScreen()
       }
      
      
       
       
-      
-
-     }
+    }
     
-    
- 
-    //  
-      
- 
-     
-    
-    
-      
-      // updateScreen()
-      
   }
    boardDiv.addEventListener("click",handleClick)
     updateScreen()
 }
 ScreenControll()
-
-function computerMoves(){
-  const cells = document.querySelectorAll('.cell')
-  let texts;
-  
-  //for(let i = 0;i <= cells;i++){
  
-    let computerMoves =  Math.floor(Math.random() * 9)
-   
-    let mark = cells[computerMoves]
-   
-   
-  for(let i=0;i<=cells.length;i++){
-      computerMoves =Math.floor(Math.random() * 9)
-      mark = cells[computerMoves]
-      if(mark.textContent == ""){
-        texts = mark
-       
-        return texts
-        console.log(texts)
-        
-      }
-      
+ 
+  
+  //Ai for the hard level module to return index
+  //function that accept board and player object(currentPlayer)
+function aiMove(){
+  let player = { man:"o",computer:"x"}
+
+  function minimax(boardState, currentPlayer){
+    let freeSpot = getEmptySpaces(boardState);
     
-    
-   }
+    //array of empty space that  doesn't  have marked yet 
+// BASE (to stop the recursion)
+if(checkWinner(boardState, player.computer) ) return { evaluation : +10 };
+if( checkWinner(boardState, player.man)      ) return { evaluation : -10 };
+if( freeSpot.length == 0                   ) return { evaluation : 0 };
+
+
+
+// save the index there evaluation or the result u would get if you move in that space
+let moves = [];
+
+// loop over the empty space and get evaluation or the result
+for( let i = 0; i < freeSpot.length; i++){
+    // index of empty space
+    let index = freeSpot[i];
    
-  //}
-    
-  return mark
-//console.log(computerMoves)
+console.log(boardState[6])
+    // find that empty space to mark it
+    let backup = boardState[index];
+
+    // mark the spot using that index with the player mark
+    boardState[index] = currentPlayer;
+
+    // save the index and it's evaluation
+    let move = {};
+    move.index = index;
+    // evaluation for the move using that index using minimax function recursion occur
+    if( currentPlayer == player.computer){
+        move.evaluation = minimax(boardState, player.man).evaluation;
+     
+    }else{
+        move.evaluation = minimax(boardState, player.computer).evaluation;
+    }
+
+    // restore the space or the board to it's unmarked state
+    boardState[index] = backup;
+  
+
+    // save the move which holds index and evaluation sing minimax in array( moves)
+    moves.push(move);
+    console.log(moves)
    
 }
-computerMoves()
- 
+
+// choose the bestmove using minimax algorithm
+/* maximizer which is the ai on it's turn should aim to highst score
+     when human turn choose lowest score
+    which limit human to win  to do that
+     loop through moves which had evaluation and choose from it
+    */
+let bestMove;
+
+    if(currentPlayer == player.computer){
+        
+        //maximizer
+        let bestEvaluation = -Infinity;
+        for(let i = 0; i < moves.length; i++){
+            if( moves[i].evaluation > bestEvaluation ){
+                bestEvaluation = moves[i].evaluation;
+                bestMove = moves[i];
+            }
+        }
+    }
+    else{
+        // minimizer the opposite of maximizer choose lowest evaluation
+        let bestEvaluation = +Infinity;
+        for(let i = 0; i < moves.length; i++){
+            if( moves[i].evaluation < bestEvaluation ){
+                bestEvaluation = moves[i].evaluation;
+                bestMove = moves[i];
+            }
+        }
+    }
+
+return bestMove;
+
   
- 
-  
+}
+
+
+//function that accept board  loop over it
+//and put the index of that free space in the array and return that array
+function getEmptySpaces(board){
+        let indeices = []
+        for(let i = 0;i  < board.length;i++){
+            if(board[i] == ""){
+              indeices.push(i)
+            }
+        }
+     return indeices;
+}
+const bestMoves = () => minimax()
+return{bestMoves}
+}
+let player = { man:"o",computer:"x"}
+let gameData = ["x","o","x",
+"x","o","x",
+"","","o"]
+//onsole.log(aiMove.bestMov(gameData,player))
 
   
    
